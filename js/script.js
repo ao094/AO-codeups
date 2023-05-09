f2a5b43972549209171b2a29fcb750071b8c26d1
ao094 <094aya.design@gmail.com>
2023-05-09 20:30:25 +0900

    JavaScript heap out of memoryの表示が出たため、修復をトライするためバックアップ用にコミット
    Signed-off-by: ao094 <094aya.design@gmail.com>

--- js/script.js
+++ js/script.js
@@ -24,14 +24,24 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
   });
 
   //ドロワーメニュー
-  $("#MenuButton").click(function () {
-    // $(".l-drawer-menu").toggleClass("is-show");
-    // $(".p-drawer-menu").toggleClass("is-show");
-    $(".js-drawer-open").toggleClass("open");
-    $(".drawer-menu").toggleClass("open");
-    $("html").toggleClass("is-fixed");
+  // $("#MenuButton").click(function () {
+  //   $(".js-drawer-open").toggleClass("open");
+  //   $(".drawer-menu").toggleClass("open");
+  //   $("html").toggleClass("is-fixed");
+  // });
 
-  });
+    //ナビバートグル
+    $('.js-hamburger').on('click', function () {
+      if ($('.js-hamburger').hasClass('is-open')) {
+        // $('.js-drawer-menu').fadeOut();
+        $('.js-drawer-menu').removeClass('is-open');
+        $(this).removeClass('is-open');
+      } else {
+        // $('.js-drawer-menu').fadeIn();
+        $('.js-drawer-menu').addClass('is-open');
+        $(this).addClass('is-open');
+      }
+    });
 
 
 
